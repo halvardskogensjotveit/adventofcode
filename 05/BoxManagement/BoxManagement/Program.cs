e1ca3b11d46bd88c9f511cf4141ad4085cb531b1
Halvard Sjøtveit <halvard.sjotveit@noaignite.com>
2022-12-05 14:56:36 +0100

    refactor and 05-2

--- 05/BoxManagement/BoxManagement/Program.cs
+++ 05/BoxManagement/BoxManagement/Program.cs
@@ -1,40 +1,73 @@
 ﻿var inputLines = File.ReadLines("C:\\dev\\repos\\adventofcode\\05\\input.txt").ToList();
-var lengthOfStackInput = 8;
-var numOfStacks = 9;
-var stacks = new Stack<string>[numOfStacks];
 
-for (int currentInputLine = lengthOfStackInput-1; currentInputLine > -1; currentInputLine--)
+var stacks9000 = CreateStacks(inputLines);
+foreach (var inputLine in inputLines.FindAll(l => l.StartsWith("move")))
 {
-    int currentInputOffset = 0;
-    for(int currentStack = 0; currentStack < stacks.Length; currentStack++)
-    {
-        if (stacks[currentStack] == null)
-            stacks[currentStack] = new Stack<string>(); 
-
-        var stackString = inputLines[currentInputLine].Substring(currentInputOffset, 3).Trim();
-        if (!string.IsNullOrWhiteSpace(stackString))
-            stacks[currentStack].Push(stackString[1].ToString());
-
-        currentInputOffset += 4;
-    }
-}
-
-foreach(var inputLine in inputLines.FindAll(l => l.StartsWith("move")))
-{
-    var commandStringArr = inputLine.Split(' ');
-    var (amount, from, to) = (int.Parse(commandStringArr[1]), int.Parse(commandStringArr[3]) - 1, int.Parse(commandStringArr[5]) - 1);
-
-
+    var (amount, from, to) = GetCommands(inputLine);
     for (int i = amount; i > 0; i--)
     {
-        stacks[to].Push(stacks[from].Pop());
+        stacks9000[to].Push(stacks9000[from].Pop());
     }
 }
 
-var topOfStackStr = "";
-foreach(var stack in stacks)
+var topOfStackStr9000 = "";
+foreach(var stack in stacks9000)
 {
-    topOfStackStr += stack.Peek();
+    topOfStackStr9000 += stack.Peek();
 }
 
-Console.WriteLine($"Top of stack string: {topOfStackStr}");
+Console.WriteLine($"Top of stack string 9000: {topOfStackStr9000}");
+
+
+var stacks9001 = CreateStacks(inputLines);
+foreach (var inputLine in inputLines.FindAll(l => l.StartsWith("move")))
+{
+    var (amount, from, to) = GetCommands(inputLine);
+    var tempStack = new List<string>();
+    for (int i = amount; i > 0; i--)
+    {
+        tempStack.Add(stacks9001[from].Pop());
+    }
+    tempStack.Reverse();
+    tempStack.ForEach(e => stacks9001[to].Push(e));
+    
+}
+
+var topOfStackStr9001 = "";
+foreach (var stack in stacks9001)
+{
+    topOfStackStr9001 += stack.Peek();
+}
+
+Console.WriteLine($"Top of stack string 9001: {topOfStackStr9001}");
+
+
+(int, int, int) GetCommands(string inputLine)
+{
+    var commandStringArr = inputLine.Split(' ');
+    return (int.Parse(commandStringArr[1]), int.Parse(commandStringArr[3]) - 1, int.Parse(commandStringArr[5]) - 1);
+}
+
+Stack<string>[] CreateStacks(List<string> inputLines)
+{
+    var lengthOfStackInput = 8;
+    var numOfStacks = 9;
+
+    var stacks = new Stack<string>[numOfStacks];
+    for (int currentInputLine = lengthOfStackInput - 1; currentInputLine > -1; currentInputLine--)
+    {
+        int currentInputOffset = 0;
+        for (int currentStack = 0; currentStack < stacks.Length; currentStack++)
+        {
+            if (stacks[currentStack] == null)
+                stacks[currentStack] = new Stack<string>();
+
+            var stackString = inputLines[currentInputLine].Substring(currentInputOffset, 3).Trim();
+            if (!string.IsNullOrWhiteSpace(stackString))
+                stacks[currentStack].Push(stackString[1].ToString());
+
+            currentInputOffset += 4;
+        }
+    }
+    return stacks;
+}
